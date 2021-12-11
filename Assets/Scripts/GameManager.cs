68008181b9c2884b950bb9f2109cb554583d771e
Matthew Grima <matthew@Matthews-MacBook-Pro.local>
2021-12-11 11:33:05 +0100

    unique key being generated and displayed to user to share

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TMPro.TMP_InputField inpGameName;
+    [SerializeField] private TMPro.TMP_InputField inpUniqueCodeToShare;
+
+    public static void LoadScene(string sSceneName)
+    {
+        SceneManager.LoadScene(sSceneName);
+    }
+
+
+    public void createNewGameInstance()
+    {
+        if (inpGameName.text != null)
+        {
+            StartCoroutine(FirebaseController.CreateGameInstance(inpGameName.text));
+        }
+    }
+
+
+
+
+
+    public void Awake()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Start":
+                break;
+
+            case "Lobby":
+                inpUniqueCodeToShare.text = FirebaseController.sUniqueKey;
+                break;
+
+            default:
+                break;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
